4e8b6152f7e294482b2c970e9354dc8348dfe0d7
norflin <norflin321@gmail.com>
2021-07-27 04:16:01 +0300

    make it better!

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,21 +1,17 @@
-import express from 'express';
 import { DEV, PROD, ACTIVE } from '../constants';
+import { startExpress } from '../utils/express';
 import { firestoreGetAll } from '../utils/firestore';
 
-const app = express();
-app.use(express.json());
-app.get('/ping', (_, res) => res.json('pong'));
+startExpress();
 
-const magic = async () => {
+const main = async () => {
   let db = await firestoreGetAll();
   if (!db) return;
-  console.log(db);
+  // console.log(db);
 };
 
 if (DEV) {
-  magic();
+  main();
 } else if (PROD && ACTIVE) {
-  magic();
+  main();
 }
-
-app.listen(process.env.PORT || 5000);
---
+++ src/utils/express.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+export const startExpress = () => {
+  const app = express();
+  app.use(express.json());
+
+  // routes
+  app.get('/ping', (_, res) => res.json('pong'));
+
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => console.log(`listen on port: ${PORT}`));
+};
